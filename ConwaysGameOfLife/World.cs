e78afadde448b33f4382672b68dd3dc5180b4638
Abdulla Mian <miana@claycorp.com>
2019-12-11 14:55:51 -0600

    game of life!

---
+++ ConwaysGameOfLife/World.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ConwaysGameOfLife
+{
+    public class World
+    {
+        public List<List<Cell>> State { get; set; } 
+
+
+        public World(List<List<Cell>> state)
+        {
+            this.State = state;
+            
+        }
+
+
+        public World Step()
+        {
+            return this;
+        }
+    }
+
+    public class Cell : IEquatable<Cell>
+    {
+        public bool Alive { get; set; }
+
+        public Cell(bool cellStatus)
+        {
+            this.Alive = cellStatus;
+        }
+        
+        public bool Equals(Cell other)
+        {
+            if (this.Alive == other.Alive)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ConwaysGameOfLifeTest/GameOfLifeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using ConwaysGameOfLife;
+using NUnit.Framework;
+
+    //At each step in time, the following transitions occur:
+
+    //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+    //Any live cell with more than three live neighbours dies, as if by overcrowding.
+    //Any live cell with two or three live neighbours lives on to the next generation.
+    //Any dead cell with exactly three live neighbours becomes a live cell.
+
+namespace ConwaysGameOfLifeTest
+{
+    public class GameOfLifeTests
+    {
+        [Test]
+        public void FirstRule()
+        {
+            // [d, d, d]
+            // [d, a, d]
+            // [d, d, d]
+            //   
+
+            var world = new World(new List<List<Cell>>
+            {
+                new List<Cell> {new Cell(false), new Cell(false), new Cell(false)},
+                new List<Cell>  {new Cell(false), new Cell(true), new Cell(false)},
+                new List<Cell> {new Cell(false), new Cell(false), new Cell(false)}
+
+            });
+
+            var nextWorld = world.Step();
+
+            var expectedState = new List<List<Cell>>
+            {
+                new List<Cell> {new Cell(false), new Cell(false), new Cell(false)},
+                new List<Cell> {new Cell(false), new Cell(false), new Cell(false)},
+                new List<Cell> {new Cell(false), new Cell(false), new Cell(false)}
+            };
+
+            // nextWorld.State to Equal expectedState
+            Assert.AreEqual(expectedState, nextWorld.State);
+        }
+    }
+}
